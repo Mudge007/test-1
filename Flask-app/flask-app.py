cfd2c211a67eba219365c347777057a4ffb121a6
Mudge007 <alistair.ross@landregistry.gsi.gov.uk>
2015-01-12 14:40:59 +0000

    added Goodbye

--- Flask-app/flask-app.py
+++ Flask-app/flask-app.py
@@ -6,5 +6,12 @@ def hello():
   a = 'Hello'
   b = 'World'
   return a+' '+b
+  
+@app.route('/goodbye')
+def goodbye():
+  a = 'Goodbye'
+  b = 'World'
+  return a+' '+b
+  
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', debug=True)
+  app.run(host='0.0.0.0', debug=True)  
